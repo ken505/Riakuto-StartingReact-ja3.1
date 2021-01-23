6729b087cd88a25a1bd80d7373f4a0da3e6198a1
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-23 14:49:59 +0900

    WIP:

--- 02-javascript/09-module/esm/modules/constants.js
+++ 02-javascript/09-module/esm/modules/constants.js
@@ -2,4 +2,7 @@ const ONE = 1;
 const TWO = 2;
 
 export { ONE, TWO };
+// ていぎ定義しておいたものをまとめてエクスポート。
+
 export const TEN = 10;
+// 定義と同時にエキスポート。
--- 02-javascript/09-module/esm/modules/math.js
+++ 02-javascript/09-module/esm/modules/math.js
@@ -1,6 +1,11 @@
 import { ONE, TWO as ZWEI } from './constants.js';
+// ONE はその名前のままインポート。
+// TWO は変数名をインポート時に ZWEI へ変更。
 
 export const plus = (n, m = ONE) => n + m;
 const times = (n, m = ZWEI) => n * m;
 
 export default times;
+// 名前なしエクスポート
+// 読み込み側で任意の名前をつけられる。
+// １モジュールにつき１つ。
