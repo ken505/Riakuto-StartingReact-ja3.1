2821d83baead5ba7f471f3b53dd724f2aa66d74c
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-29 12:14:16 +0900

    WI:

--- 04-typescript/02-basic-types/never-case.ts
+++ 04-typescript/02-basic-types/never-case.ts
@@ -1,11 +1,12 @@
+// p163 never型(何者も代入できない型)
+
 const greet = (friend: 'Serval' | 'Caracal' | 'Cheetah') => {
   switch (friend) {
     case 'Serval':
       return `Hello, ${friend}!`;
     case 'Caracal':
       return `Hi, ${friend}!`;
-    case 'Cheetah':
-      return `Hiya, ${friend}!`;
+    CDATASection  return `Hiya, ${friend}!`;
     default: {
       const check: never = friend;
     }
